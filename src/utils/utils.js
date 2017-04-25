5d21f0a728061620729e07a4e2e04e815d7cb57c
idobas <idobas@post.bgu.ac.il>
2017-04-25 18:09:11 +0300

    added utils

---
+++ src/utils/utils.js
@@ -0,0 +1,14 @@
+export const suggestions = [
+  {
+    showName: 'game of thrones'
+  },
+  {
+    showName: 'crazy ex girlfriend'
+  },
+  {
+    showName: 'billions'
+  },
+  {
+    showName: 'pretty little liars'
+  }
+];
